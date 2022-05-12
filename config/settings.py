2ca6350d587f4c3eef9a9935f417f26eacd266b0
jaehoon <07jjh1129@naver.com>
2022-05-12 14:21:56 +0900

    Path error correction

--- config/settings.py
+++ config/settings.py
@@ -71,9 +71,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
     }
 }
 

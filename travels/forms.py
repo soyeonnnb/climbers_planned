4ae9f37d572660d634278506302c599505e95f17
soyeonnnb <la28s5d@naver.com>
2022-05-13 19:12:24 +0900

    feat: new_place 초안

--- travels/forms.py
+++ travels/forms.py
@@ -3,22 +3,17 @@ from .models import Travel, Place, Lodging
 
 
 class CreateTravelForm(forms.Form):
-    city = forms.CharField() #여행이름
+    def __init__(self, *args, **kwargs):
+        site = []
+        sites = Place.objects.all()
+        super(CreateTravelForm, self).__init__(*args, **kwargs)
+        counter = 1
+        # for s in sites:
+        for s in range(5):  # 여행장소 5개로 임의지정
+            self.fields["site-" + str(counter)] = forms.CharField(label="site")
+            counter += 1
+
+    city = forms.CharField()  # 여행이름
     start_date = forms.DateField()
     end_date = forms.DateField()
     lodging = forms.CharField()
-    site = forms.CharField() #여행지 추가
-
-    # class Meta:
-    #     model = Travel
-    #     fields = ['name', 'start_date', 'end_date']
-
-# class PlaceForm(forms.ModelForm):
-#     class Meta:
-#         model = Place
-#         fields = ['name']
-
-# class LodgingForm(forms.ModelForm):
-#     class Meta:
-#         model = Lodging
-#         fields = ['name']
--- travels/views.py
+++ travels/views.py
@@ -16,28 +16,45 @@ def createtravel(request):
             start_date = form.cleaned_data.get("start_date")
             end_date = form.cleaned_data.get("end_date")
             lodging = form.cleaned_data.get("lodging")
-            site = form.cleaned_data.get("site")
             count_date = (end_date - start_date).days + 1
-
             new_travel = models.Travel.objects.create(
                 name=city, start_date=start_date, end_date=end_date, user=user
             )
-            models.Lodging.objects.create(
-                travel=new_travel,
-                name=lodging,
-                latitude=random.uniform(0, 5),
-                longitude=random.uniform(0, 5),
-            )
-            for i in range(10):
+            for i in range(1, 6):
+                site_name = "site-" + str(i)
+                site = form.cleaned_data.get(site_name)
                 models.Place.objects.create(
                     travel=new_travel,
-                    name=site + str(i),
-                    day=random.randint(1, count_date),
-                    latitude=random.uniform(0, 5),
-                    longitude=random.uniform(0, 5),
-                    order=0,
+                    name=site,
+                    day=1,
+                    order=1,
+                    latitude=1,
+                    longitude=1,
                 )
-            aco.aco_run(new_travel, count_date)
+            # new_place.save()
+            # new_place = models.Place.objects.create(
+            #     travel=new_travel,
+            #     name=site, day=day, order=order
+            # )
+
+            # 여기부터 aco.aco_run 까지는 소연님 aco 코드
+            # new_lodging = models.Lodging.objects.create(
+            #     travel=new_travel,
+            #     name=lodging,
+            #     latitude=random.uniform(0, 5),
+            #     longitude=random.uniform(0, 5),
+            # )
+            # for i in range(10):
+            #     new_place = models.Place.objects.create(
+            #         travel=new_travel,
+            #         name=site + str(i),
+            #         day=random.randint(1, count_date),
+            #         order=0,
+            #         latitude=random.uniform(0, 5),
+            #         longitude=random.uniform(0, 5),
+            #     )
+            #     new_place.save()
+            # aco.aco_run(new_travel, count_date)
             return redirect("travels:checkpath", pk=new_travel.pk)
     else:
         form = forms.CreateTravelForm()
@@ -52,4 +69,5 @@ def createtravel(request):
 
 def checkpath(request, pk):
     travel = models.Travel.objects.get(pk=pk)
-    return render(request, "travels/checkpath.html", {"travel": travel})
+    place = models.Place.objects.filter(travel=pk).order_by("order")
+    return render(request, "travels/checkpath.html", {"travel": travel, "place": place})

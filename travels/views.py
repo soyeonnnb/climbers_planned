8682d1a8e10105ae04b5a3b36ea1e3aabcfe2864
leta-ha <letaha@naver.com>
2022-05-26 20:35:41 +0900

    feat: 여행일자별로 여행지 띄우기(여행지확인페이지)

--- travels/views.py
+++ travels/views.py
@@ -72,11 +72,7 @@ def checkpath(request, pk):
 
 
 def savepath(request):  # 경로 저장
-    # 경로 저장의 경우, 여행지 추가하는 과정에서 이미 db를 넘겨주므로 db에 저장할 필요 없는 것 같은데.. 맞나요?
-    # 시퀀스 다이어그램 상 다시 저장해주어야 하긴 하는데.. 갱신 아닌지.. 근데 갱신할 필요가 없는 것 같아서요
-    # travel = models.Travel.objects.get(pk=pk)
-    # place = models.Place.objects.filter(travel=pk).order_by('order')
-    # #db 저장 안 해도 되나?
+    # 경로 저장의 경우, 여행지 추가하는 과정에서 이미 db를 넘겨줌.
     return redirect("core")
 
 
@@ -86,16 +82,25 @@ def checktravel(request, pk):
     places = []
     for p in models.Place.objects.filter(travel=pk):
         places.append(p)
+    count_date = (travel.end_date - travel.start_date).days + 1    
+    chk_day = [[] for _ in range(count_date)]
+    for i in places:
+        chk_day[i.day-1].append(i) 
+    for i in range(len(chk_day)):
+        chk_day[i].insert(0, i+1)
+    print(chk_day)
+    
     # places = sorted(places, key=models.Place.day)
     return render(
         request,
         "travels/checktravel.html",
-        {"travel": travel, "lodging": lodging, "places": places},
+        {"travel": travel, "lodging": lodging, "places": places, "chk_day": chk_day},
     )
 
 
 def addplace(request):
     return render(request, "travels/addplace.html")
+
 
 def updatetravel(request, pk):
     travel = get_object_or_404(models.Travel, pk=pk)
@@ -120,14 +125,15 @@ def updatetravel(request, pk):
             travel.end_date = travel_end_date
             lodging.name = lodging_name
 
-            for place in places:
-                place_name = placeformset.cleaned_data["place_name"]
+            for place in placeformset:
+                place_name = place.cleaned_data["place_name"]
                 place.name = place_name
 
             travel.save()
             lodging.save()
             places.save()
-        return redirect("travels:checktravel", kwargs={"pk": pk})
+        return redirect("travels:checktravel", kwargs={"pk": pk}) 
+        #return redirect("travels:checktravel", {"travel": travel, "lodging": lodging, "places": places},
 
     else:
         travelform = forms.TravelModelForm(instance=travel)
@@ -142,7 +148,6 @@ def updatetravel(request, pk):
             "lodgingform": lodgingform,
             "placeformset": placeformset,
         },
-        context_instance=RequestContext(request),
     )
 
 
